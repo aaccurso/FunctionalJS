fc812f2b284ff93dcffb33a8ec2ffd06c7792ac8
aaccurso <accurso.alan@gmail.com>
2014-06-23 15:37:44 -0300

    Adds some tests for _.compose

--- spec/compose.js
+++ spec/compose.js
@@ -1,6 +1,34 @@
 'use strict';
 describe('_.compose', function() {
+
+  var f = function (num) {
+    return num + 2;
+  };
+  var g = function (num) {
+    return num * 2;
+  };
+
   it('should be defined', function() {
     expect(_.compose).toBeDefined();
   });
+
+  it('should define a function that calls another function with args', function() {
+    var proxy = function (fn) {
+      return function () {
+        return fn.apply(null, arguments);
+      }
+    }
+    expect(proxy(f)(2)).toBe(4);
+  });
+
+  it('should compose fog', function() {
+    var fog = _.compose(f, g);
+    expect(fog(3)).toBe(10);
+  });
+
+  it('should compose gof', function() {
+    var gof = _.compose(g, f);
+    expect(gof(3)).toBe(12);
+  });
+
 });
