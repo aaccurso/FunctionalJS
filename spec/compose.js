1fa13f31ecca3a1b554b43319ad92d8ef177c3e2
aaccurso <accurso.alan@gmail.com>
2014-06-24 10:05:45 -0300

    Fixes reverse() bug (it's mutable!)

--- spec/compose.js
+++ spec/compose.js
@@ -24,21 +24,25 @@ describe('_.compose', function() {
   it('should compose f', function() {
     var composed_f = _.compose(f);
     expect(composed_f(3)).toBe(5);
+    expect(composed_f(5)).toBe(7);
   });
 
   it('should compose g', function() {
     var composed_g = _.compose(g);
     expect(composed_g(3)).toBe(6);
+    expect(composed_g(5)).toBe(10);
   });
 
   it('should compose fog', function() {
     var fog = _.compose(f, g);
     expect(fog(3)).toBe(8);
+    expect(fog(4)).toBe(10);
   });
 
   it('should compose gof', function() {
     var gof = _.compose(g, f);
     expect(gof(3)).toBe(10);
+    expect(gof(4)).toBe(12);
   });
 
   it('should compose a multi argument function h', function() {
@@ -47,5 +51,6 @@ describe('_.compose', function() {
     }
     var goh = _.compose(g, h);
     expect(goh(3, 2)).toBe(10);
+    expect(goh(4, 3)).toBe(14);
   });
 });
---
+++ spec/currify.js
@@ -0,0 +1,45 @@
+'use strict';
+describe('_.currify', function() {
+
+  var f = function (num1, num2) {
+    return num1 + num2;
+  };
+  var g = function (num1, num2) {
+    return num1 * num2;
+  };
+
+  it('should be defined', function() {
+    expect(_.currify).toBeDefined();
+  });
+
+  it('should currify f manually', function() {
+    var curried_f = function (num1) {
+      return function (num2) {
+        return f(num1, num2);
+      }
+    }
+    expect(curried_f(3)(4)).toBe(7);
+  });
+
+  it('should currify f', function() {
+    var curried_f = _.currify(f);
+    expect(curried_f(3)(4)).toBe(7);
+    expect(curried_f(4)(5)).toBe(9);
+  });
+
+  it('should currify g', function() {
+    var curried_g = _.currify(g);
+    expect(curried_g(3)(4)).toBe(12);
+    expect(curried_g(4)(5)).toBe(20);
+  });
+
+  it('should currify multi argument function h', function() {
+    var h = function (arg1, arg2, arg3) {
+      return arg1 + arg2 + arg3;
+    }
+    var curried_h = _.currify(h);
+    var curried_h_add5 = curried_h(5);
+    expect(curried_h_add5(3)(2)).toBe(10);
+    expect(curried_h_add5(34)(26)).toBe(55);
+  });
+});
