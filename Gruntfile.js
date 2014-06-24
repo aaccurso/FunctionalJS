e4d63829dd133e115720f3f13981e6b376da9450
aaccurso <accurso.alan@gmail.com>
2014-06-24 00:00:10 -0300

    Implementation of _.compose with reverse and reduce

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,36 @@ module.exports = function (grunt) {
   "use strict";
 
   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json')
-    , jshint: {
-        all: [
-            "Gruntfile.js"
-          , "lib/**/*.js"
-          , "spec/**/*.js"
-        ]
-      }
-    , jasmine: {
-        src: "lib/**/*.js"
-      , options: {
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: [
+          "Gruntfile.js",
+          "lib/**/*.js",
+          "spec/**/*.js"
+      ]
+    },
+    jasmine: {
+      dev: {
+        src: "lib/**/*.js",
+        options: {
           specs: "spec/**/*.js"
+        }
       }
+    },
+    watch: {
+      files: [
+        'lib/**/*.js',
+        'spec/**/*.js',
+        'Gruntfile.js'
+      ],
+      tasks: ['jasmine']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('test', ['jshint', 'jasmine']);
-  grunt.registerTask('default', ['jasmine']);
+  grunt.registerTask('default', ['jasmine', 'watch']);
 };
--- spec/compose.js
+++ spec/compose.js
@@ -26,14 +26,19 @@ describe('_.compose', function() {
     expect(composed_f(3)).toBe(5);
   });
 
+  it('should compose g', function() {
+    var composed_g = _.compose(g);
+    expect(composed_g(3)).toBe(6);
+  });
+
   it('should compose fog', function() {
     var fog = _.compose(f, g);
-    expect(fog(3)).toBe(10);
+    expect(fog(3)).toBe(8);
   });
 
   it('should compose gof', function() {
     var gof = _.compose(g, f);
-    expect(gof(3)).toBe(12);
+    expect(gof(3)).toBe(10);
   });
 
 });
